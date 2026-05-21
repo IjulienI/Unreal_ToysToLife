98a016cac7047bdebb847fe78a6ce9bb7459d5ca
IjulienI <89165787+IjulienI@users.noreply.github.com>
2026-05-21 17:36:01 +0200

    wip : Adding Editor Plugin

---
+++ Source/ToysToLifeEditor/Private/ToysToLifeEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "ToysToLifeEditor.h"
+
+#define LOCTEXT_NAMESPACE "FToysToLifeEditorModule"
+
+void FToysToLifeEditorModule::StartupModule()
+{
+    
+}
+
+void FToysToLifeEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FToysToLifeEditorModule, ToysToLifeEditor)
---
+++ Source/ToysToLifeEditor/ToysToLifeEditor.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class ToysToLifeEditor : ModuleRules
+{
+    public ToysToLifeEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+            }
+        );
+    }
+}
